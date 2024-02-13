9770eeb62bd053c5225cb4510f50f22fa69cc143
mayurfine <mayur.pondkule@fineshift.com>
2024-02-13 12:55:00 +0530

    first commit

---
+++ src/server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const app=express()
+
+
+app.get('/',(req,res)=>{
+    res.send("welcome to my app")
+})
+
+app.listen(3000,() =>{
+    console.log("app listening on port 3000")
+});
